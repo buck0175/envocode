d9c7b4cee96d5f8fac7e583dd7548de5acb56ce5
Ben Buck <benbuck@Bens-MacBook-Pro-2.local>
2018-07-26 16:58:00 -0400

    database fix

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ var app = express();
 // Mongoose Database
 // mongoose.connect('mongodb://localhost/envocode');
 
-// mongoose.connect('mongodb://envocode:@ds141960.mlab.com:41960/envocode');
+mongoose.connect('mongodb://envocode:Pickle6134@ds141960.mlab.com:41960/envocode');
+
+
 
 
 
